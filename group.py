0e9be4963ce081a58af038b2b9c97fc2d376b041
Ian Davison <idavison157@gmail.com>
2021-09-15 14:41:56 -0600

    commit 5

--- group.py
+++ group.py
@@ -8,7 +8,8 @@ def ian():
 	print("I am a junior"):
 	print("I am a CSCI major"):
 	print("I like to play video games and ski"):
-	print("")
+	print("I grew up in Arcata CA"):
+	print("I played varsity tennis for 4 years in high school"):
 
 
 
